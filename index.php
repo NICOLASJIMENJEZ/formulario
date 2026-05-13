b83a573db77eced000a0f52801ab41b61d5cb93a
Nicolas Jimenez <nicolasjimenezguzman1@gmail.com>
2026-05-13 07:45:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -398,7 +398,7 @@ if ($dbConnected) {
                       <option value="">Seleccione una hora</option>
                       <option value="09:00">09:00 AM</option>
                       <option value="14:00">02:00 PM</option>
-                      <option value="16:30">05:30 PM</option>
+                      <option value="16:30">04:30 PM</option>
                     </select>
                   </div>
 
@@ -503,7 +503,7 @@ if ($dbConnected) {
   'Psicología'
 ],
 
-'16:30': [
+'4:30': [
   'Administración de Empresas',
   'Contaduría Pública',
   'Ingeniería de Sistemas',
